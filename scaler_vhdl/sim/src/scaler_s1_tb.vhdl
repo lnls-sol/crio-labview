9ae37ec488f6ed939fe8a18d207b99c5531e66bd
Dawood <d.najjar@gmail.com>
2018-11-07 15:32:39 -0200

    Added test benches for scaler modules.

---
+++ scaler_vhdl/sim/src/scaler_s1_tb.vhdl
@@ -0,0 +1,124 @@
+--------------------------------------------------------------------------------
+--  File:       postobit_block.vhd
+--  Desc:       strips one of the position bits and outputs it
+--
+--------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use std.env.all;
+
+library work;
+
+
+entity scaler_s1_tb is
+end scaler_s1_tb;
+
+architecture rtl of scaler_s1_tb is
+
+    signal clk_10              : std_logic := '0';
+	signal reset_i             : std_logic := '0';
+	signal fpga_enable_i       : std_logic := '1';
+	signal jump_to_end_i       : std_logic;
+	signal scaler_enable_i     : std_logic; -- This starts and stops the counters (and holds until enable is set to 1 again. Setting to one again, resets counters)
+	signal gate_i              : std_logic; -- This decides whether simple or preset is chosen
+	signal preset_value_i      : std_logic_vector(31 downto 0) := (others => '0');	
+	signal counter_o           : std_logic_vector(31 downto 0);
+	signal status_o            : std_logic_vector(1  downto 0);
+	signal done_o              : std_logic;
+
+
+    constant   simple       : std_logic := '0';    
+    constant   preset       : std_logic := '1';  
+    
+    -- Procedure for clock generation
+    procedure clk_gen(signal clk : out std_logic; constant FREQ : real) is
+        constant PERIOD    : time := 1 sec / FREQ;        -- Full period
+        constant HIGH_TIME : time := PERIOD / 2;          -- High time
+        constant LOW_TIME  : time := PERIOD - HIGH_TIME;  -- Low time; always >= HIGH_TIME
+    begin
+    -- Check the arguments
+        assert (HIGH_TIME /= 0 fs) report "clk_plain: High time is zero; time resolution to large for frequency" severity FAILURE;
+    -- Generate a clock cycle
+        loop
+          clk <= '1';
+          wait for HIGH_TIME;
+          clk <= '0';
+          wait for LOW_TIME;
+        end loop;
+    end procedure;
+
+    -- Procedure for clock generation
+    procedure wait_posedge(signal clk : in std_logic) is
+    begin
+        wait until clk'event and clk='1';
+    end procedure;    
+begin
+
+process begin
+        clk_gen(clk_10, 10.000E6);  
+end process;
+
+
+process begin
+
+------------------- Test 1 --- Count to 256
+        reset_i <= '1';
+        wait_posedge(clk_10);
+        wait_posedge(clk_10);
+        reset_i <= '0';
+	    jump_to_end_i <= '0';
+        gate_i <= '1';
+	    preset_value_i(8 downto 0) <=  "100000000";
+	    scaler_enable_i <= '1';
+        wait until done_o = '1';
+	    scaler_enable_i <= '0';
+        wait_posedge(clk_10);
+        wait_posedge(clk_10);
+        
+------------------- Test 2 --- Count infinitely
+        gate_i <= '0';
+	    preset_value_i(8 downto 0) <=  "100000000";
+	    scaler_enable_i <= '1';
+        wait for 1000000 ps;
+        jump_to_end_i <= '1';
+        wait until done_o = '1';      
+	    scaler_enable_i <= '0';
+	    wait until done_o = '0';
+        wait_posedge(clk_10);
+        wait_posedge(clk_10);        	    
+        stop(0);
+
+------------------- Test 3 --- disable enable and hold
+        gate_i <= '0';
+	    preset_value_i(8 downto 0) <=  "100000000";
+	    scaler_enable_i <= '1';
+        wait for 1000000 ps;
+        scaler_enable_i <= '0';
+	    wait until done_o = '0';
+        wait_posedge(clk_10);
+        wait_posedge(clk_10);        	    
+        stop(0);        
+end process;
+
+
+
+scaler : entity work.scaler_s1
+port map (
+
+    clk_i               => clk_10,
+	reset_i             => reset_i,
+	fpga_enable_i       => fpga_enable_i,
+	scaler_enable_i     => scaler_enable_i,
+	jump_to_end_i       => jump_to_end_i,
+	gate_i              => gate_i,
+	preset_value_i      => preset_value_i, 
+	counter_o           => counter_o,
+	status_o            => status_o, 
+	done_o              => done_o
+);
+
+
+end rtl;
+
--- scaler_vhdl/src/scaler_s1.vhdl
+++ scaler_vhdl/src/scaler_s1.vhdl
@@ -11,18 +11,14 @@ use ieee.numeric_std.all;
 use ieee.std_logic_1164.all;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
-entity scaler_pulse_counter is
+entity scaler_s1 is
 port (
     clk_i               : in  std_logic;
 	reset_i             : in  std_logic;
 	fpga_enable_i       : in  std_logic;
-	is_s1               : in  std_logic;
 	jump_to_end_i       : in  std_logic;
 	scaler_enable_i     : in  std_logic; -- This starts and stops the counters (and holds until enable is set to 1 again. Setting to one again, resets counters)
 	gate_i              : in  std_logic; -- This decides whether simple or preset is chosen
-	pulse_i             : in  std_logic; -- This is the signal to be evaluated
-	preset_type_i       : in  std_logic; -- This is the preset type in case preset is chosen
-	preset_time_i       : in  std_logic_vector(31 downto 0); -- this is the number of ticks that if reached will stop the counter
 	preset_value_i      : in  std_logic_vector(31 downto 0);	
 	counter_o           : out std_logic_vector(31 downto 0);
 	status_o            : out std_logic_vector(1  downto 0);
@@ -32,43 +28,33 @@ port (
 );
 
 
-end scaler_pulse_counter;
+end scaler_s1;
 
-architecture rtl of scaler_pulse_counter is
+architecture rtl of scaler_s1 is
 
-    TYPE state_type is (ST_RESET_ON_EXIT, ST_SIMPLE, ST_TIME_PRESET, ST_PRESET_VALUE, ST_DONE);
+    TYPE state_type is (ST_RESET_ON_EXIT, ST_SIMPLE, ST_PRESET_VALUE, ST_DONE);
 
     constant   stat_counting  : std_logic_vector(1  downto 0) := "01";
     constant   stat_done      : std_logic_vector(1  downto 0) := "10";    
     constant   stat_idle      : std_logic_vector(1  downto 0) := "00";   
 
-    constant   preset_time  : std_logic := '1';    
-    constant   preset_val   : std_logic := '0';  
 
     constant   simple       : std_logic := '0';    
     constant   preset       : std_logic := '1';  
           
     
-    signal     preset_time_r  : std_logic_vector(31 downto 0);
-    signal     preset_time_n  : std_logic_vector(31 downto 0);
     signal     preset_val_r   : std_logic_vector(31 downto 0);
     signal     preset_val_n   : std_logic_vector(31 downto 0);    
-    signal     tick_cntr_r    : std_logic_vector(31 downto 0);
-    signal     tick_cntr_n    : std_logic_vector(31 downto 0);
     signal     pulse_cntr_r   : std_logic_vector(31 downto 0);
     signal     pulse_cntr_n   : std_logic_vector(31 downto 0);    
 	signal     state_r        : state_type;    
 	signal     state_n        : state_type;    
-	signal     pulse_f        : std_logic;	
-	signal     pulse_2f       : std_logic;	
-	signal     increment      : std_logic;	
 	signal     status         : std_logic_vector(1 downto 0);
 	signal     done           : std_logic;
 	
 begin 
 
 	counter_o <= pulse_cntr_r;
-	increment <= '1' when (((pulse_2f = '0') and (pulse_f = '1')) = true) else '0';
     status_o <= status;
     done_o <= done;
     
@@ -77,27 +63,15 @@ begin
         if rising_edge(clk_i) then
             if (reset_i = '1') then
                 preset_val_r <= (others => '0');
-                preset_time_r<= (others => '0');
                 state_r      <= ST_RESET_ON_EXIT;
-                pulse_f      <= '0';
-                pulse_2f     <= '0'; 
-                tick_cntr_r  <= (others => '0');
                 pulse_cntr_r <= (others => '0');
             elsif (fpga_enable_i = '0') then
                 preset_val_r <= (others => '0');
-                preset_time_r<= (others => '0');
                 state_r      <= ST_RESET_ON_EXIT;
-                pulse_f      <= '0';
-                pulse_2f     <= '0';
-                tick_cntr_r  <= (others => '0');
                 pulse_cntr_r <= (others => '0');                
 			else
                 preset_val_r <= preset_val_n;
-                preset_time_r<= preset_time_n;
                 state_r      <= state_n;                				
-                pulse_f      <= pulse_i;
-                pulse_2f     <= pulse_f;
-                tick_cntr_r  <= tick_cntr_n;
                 pulse_cntr_r <= pulse_cntr_n;                
             end if;
         end if;
@@ -105,31 +79,23 @@ begin
     
    
     
-    process (state_r, pulse_cntr_r, tick_cntr_r, increment, preset_time_i, preset_value_i, preset_time_r, preset_val_r, jump_to_end_i)
+    process (state_r, pulse_cntr_r, preset_value_i, preset_val_r, jump_to_end_i, scaler_enable_i, gate_i)
     begin
-        tick_cntr_n <= tick_cntr_r;
         state_n <= state_r;
         pulse_cntr_n <= pulse_cntr_r;
         status <= stat_idle;
         done <= '0';
-        preset_time_n <= preset_time_r;
         preset_val_n <= preset_val_r;
         case state_r is
         
             when ST_RESET_ON_EXIT  => 
                 if (scaler_enable_i = '1') then
                     pulse_cntr_n  <= (others => '0');
-                    tick_cntr_n   <= (others => '0');
-                    preset_time_n <= preset_time_i;                    
                     preset_val_n  <= preset_value_i;                    
                     if (gate_i = simple) then
                         state_n <= ST_SIMPLE;
                     else
-                        if (preset_type_i = preset_time) then
-                            state_n <= ST_TIME_PRESET;
-                        else
-                            state_n <= ST_PRESET_VALUE;
-                        end if;
+                        state_n <= ST_PRESET_VALUE;
                     end if;
                 else
                     state_n <= ST_RESET_ON_EXIT;
@@ -137,50 +103,26 @@ begin
                 
             when ST_SIMPLE   => 
                 status <= stat_counting;
-                if (jump_to_end_i = '1') then
-                    state_n <= ST_DONE; 
+                if (scaler_enable_i = '1') then
+                    pulse_cntr_n <= pulse_cntr_r + 1;
+                    if (jump_to_end_i = '1') then
+                        state_n <= ST_DONE; 
+                    end if;
                 else
-                    if (scaler_enable_i = '1') then
-                        if (increment = '1' or is_s1 = '1') then
-                            pulse_cntr_n <= pulse_cntr_r + 1;
-                        end if;
-                    else
-                        state_n <= ST_RESET_ON_EXIT;      
-                    end if;
+                    state_n <= ST_RESET_ON_EXIT;      
                 end if;
-            when ST_TIME_PRESET  => 
-                status <= stat_counting;
-                if (jump_to_end_i = '1') then
-                    state_n <= ST_DONE; 
-                else                
-                    if (scaler_enable_i = '1') then
-                        if (increment = '1' or is_s1 = '1') then
-                            pulse_cntr_n <= pulse_cntr_r + 1;
-                        end if;
-                        tick_cntr_n <= tick_cntr_r + 1;
-                        if (tick_cntr_r + 1 >= preset_time_r) then
-                            state_n <= ST_DONE; 
-                        end if;
-                    else
-                        state_n <= ST_RESET_ON_EXIT;      
-                    end if;
-                end if;
-
             when ST_PRESET_VALUE  => 
                 status <= stat_counting;
-                if (jump_to_end_i = '1') then
-                    state_n <= ST_DONE; 
-                else                
-                    if (scaler_enable_i = '1') then
-                        if (increment = '1' or is_s1 = '1') then
-                            pulse_cntr_n <= pulse_cntr_r + 1;
-                        end if;
-                        if (pulse_cntr_r >= preset_val_r) then
-                            state_n <= ST_DONE; 
-                        end if;
-                    else
-                        state_n <= ST_RESET_ON_EXIT;      
+                if (scaler_enable_i = '1') then
+                    pulse_cntr_n <= pulse_cntr_r + 1;
+                    if (pulse_cntr_r + 1 >= preset_val_r) then
+                        state_n <= ST_DONE; 
                     end if;
+                    if (jump_to_end_i = '1') then
+                        state_n <= ST_DONE; 
+                    end if;
+                else
+                    state_n <= ST_RESET_ON_EXIT;      
                 end if;
             when ST_DONE  => 
                 done <= '1';
