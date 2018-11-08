7978cfe15a6a915e69c0fdd86b474259271f30d7
Dawood <d.najjar@gmail.com>
2018-11-08 09:53:42 -0200

    Completed scaler_64. Compiles, but not tested.

--- scaler_vhdl/src/scaler_64.vhdl
+++ scaler_vhdl/src/scaler_64.vhdl
@@ -20,7 +20,7 @@ port (
     clk_i               : in  std_logic;
 	reset_i             : in  std_logic;
 	fpga_enable_i       : in  std_logic;
-	jump_to_end_i       : in  std_logic;
+	one_shot_i          : in  std_logic;
 	scaler_enable_i     : in  std_logic; 
 	gate_i              : in  arraySTDLV1(0 to 63);
 	pulse_i             : in  arraySTDLV1(0 to 63);
@@ -34,11 +34,58 @@ port (
 end scaler64;
 
 architecture rtl of scaler64 is
-
+    signal scaler_enable       : std_logic;
+    signal jump_to_end         : std_logic;
+    signal done                : std_logic_vector(63 downto 0);
 	
 begin 
 
 
+
+    scaler_ctrl : entity work.scaler_ctrl
+    port map (
+        clk_i               => clk_i,
+	    reset_i             => reset_i,
+	    fpga_enable_i       => fpga_enable_i,
+	    one_shot            => one_shot_i,
+	    jump_to_end_o       => jump_to_end,
+	    scaler_enable_i     => scaler_enable_i, 
+	    scaler_enable_o     => scaler_enable, 
+	    done_i              => done
+
+    );
+
+    scaler_s1 : entity work.scaler_s1
+    port map (
+
+        clk_i               => clk_i,
+	    reset_i             => reset_i,
+	    fpga_enable_i       => fpga_enable_i,
+	    scaler_enable_i     => scaler_enable,
+	    jump_to_end_i       => jump_to_end,
+	    gate_i              => gate_i(0),
+	    preset_value_i      => preset_value_i(0), 
+	    counter_o           => counter_o(0),
+	    done_o              => done(0)
+    );
+
+   G_1 : for I in 1 to 63 generate
+    scaler_sx : entity work.scaler_sx
+    port map (
+
+        clk_i               => clk_i,
+	    reset_i             => reset_i,
+	    fpga_enable_i       => fpga_enable_i,
+	    scaler_enable_i     => scaler_enable,
+	    jump_to_end_i       => jump_to_end,
+	    gate_i              => gate_i(I),
+	    pulse_i             => pulse_i(I),
+	    preset_value_i      => preset_value_i(I), 
+	    counter_o           => counter_o(I),
+	    done_o              => done(I)
+    );
+         end generate;
+         
 
 
 end architecture;
--- scaler_vhdl/src/scaler_ctrl.vhdl
+++ scaler_vhdl/src/scaler_ctrl.vhdl
@@ -20,11 +20,10 @@ port (
 	reset_i             : in  std_logic;
 	fpga_enable_i       : in  std_logic;
 	one_shot            : in  std_logic;
-	jump_to_end_o       : in  std_logic;
 	scaler_enable_i     : in  std_logic; 
+	done_i              : in  std_logic_vector(63 downto 0);
 	scaler_enable_o     : out std_logic;
-	done_i              : in  std_logic_vector(63 downto 0)
-
+	jump_to_end_o       : out std_logic
 );
 
 
@@ -35,10 +34,14 @@ architecture rtl of scaler_ctrl is
     signal done : std_logic;
     
 	signal     state_r        : state_type;    
-	signal     state_n        : state_type;    	
+	signal     state_n        : state_type;  
+	signal     jump_to_end    : std_logic;  
+    constant zeros : std_logic_vector(done_i'range) := (others => '0');
+    
 begin 
 
-    done <= '0' when (done_i = (others => '0')) else '1';
+    jump_to_end_o <= jump_to_end;
+    done <= '0' when (done_i = zeros) else '1';
 
     process(clk_i)
     begin
@@ -56,47 +59,50 @@ begin
     process (state_r, scaler_enable_i, done)
     begin
         state_n <= state_r;
-        jump_to_end_o <= '0';
+        jump_to_end <= '0';
         scaler_enable_o <= scaler_enable_i;
         case state_r is
             when ST_ONESHOT  => 
                 if (one_shot = '0') then
                     state_n <= ST_AUTOCOUNT;
-                else if (scaler_enable_i = '1') then
+                elsif (scaler_enable_i = '1') then
                     state_n <= ST_ONESHOTCOUNTING;
                 end if;
+               
             when ST_AUTOCOUNT  =>
                 if (one_shot = '1') then
                     state_n <= ST_ONESHOT;
-                else if (scaler_enable_i = '1') then
+                elsif (scaler_enable_i = '1') then
                     state_n <= ST_AUTOCOUNTING;
                 end if;            
             when ST_AUTOCOUNTING  => 
                 if (one_shot = '1') then
                     state_n <= ST_ONESHOT;
-                else if (done = '1') then
+                elsif (done = '1') then
                     state_n <= ST_AUTOCOUNTING_DONE;
-                else if (scaler_enable_i = '0') then
+                elsif (scaler_enable_i = '0') then
                     state_n <= ST_AUTOCOUNT;
                 end if;            
             when ST_AUTOCOUNTING_DONE  => 
-                jump_to_end_o <= '1';
+                jump_to_end <= '1';
                 state_n <= ST_RESET_COUNTERS;           
             when ST_RESET_COUNTERS  => 
-                jump_to_end_o <= '1';
+                jump_to_end <= '1';
                 scaler_enable_o <= '0';
                 state_n <= ST_AUTOCOUNTING;                 
             when ST_ONESHOTCOUNTING  => 
                 if (one_shot = '0') then
                     state_n <= ST_AUTOCOUNT;
-                else if (done = '1') then
+                elsif (done = '1') then
                     state_n <= ST_ONESHOTCOUNTING_DONE;
-                else if (scaler_enable_i = '0') then
+                elsif (scaler_enable_i = '0') then
                     state_n <= ST_ONESHOT;
                 end if;            
             when ST_ONESHOTCOUNTING_DONE  =>  
-                jump_to_end_o <= '1'; 
-                state_n <= ST_ONESHOT;           
+                jump_to_end <= '1'; 
+                state_n <= ST_ONESHOT;  
+            when others => 
+                state_n <= ST_ONESHOT;                         
         end case;
     end process;
 end architecture;
